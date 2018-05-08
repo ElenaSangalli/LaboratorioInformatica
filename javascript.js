e58abeda0842c8c9a8c91a44f36c0d3100c874fe
ElenaSangalli <sangallielena10@gmail.com>
2018-05-08 16:08:01 +0200

    Modifica
    ciao

---
+++ javascript.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="scripts/jquery-1.4.3.min.js"></script>
+<script type="text/javascript" src="scripts/jquery.nivo.slider.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+  $('#slider').nivo.slider();
+});
+</script>
