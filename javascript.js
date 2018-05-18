006e298052aad8f84028ebed4705e75541eeed31
ElenaSangalli <sangallielena10@gmail.com>
2018-05-18 11:13:37 +0200

    ho fatto qualcosa

--- javascript.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript" src="scripts/jquery-1.4.3.min.js"></script>
-<script type="text/javascript" src="scripts/jquery.nivo.slider.js"></script>
-<script type="text/javascript">
-$(window).load(function() {
-  $('#slider').nivo.slider();
-});
-</script>
